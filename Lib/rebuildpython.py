3e5e13f0a003da866547b4d1c963f5e3190edf89
Sourcery AI <>
2023-04-02 16:15:16 +0000

    'Refactored by Sourcery'

--- Lib/rebuildpython.py
+++ Lib/rebuildpython.py
@@ -21,8 +21,7 @@ def find_files(directory, pattern):
     for root, dirs, files in os.walk(directory):
         for basename in files:
             if fnmatch.fnmatch(basename, pattern):
-                filename = os.path.join(root, basename)
-                yield filename
+                yield os.path.join(root, basename)
 
 
 def getPythonInitFunctions(compiler, filename):
